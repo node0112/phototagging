d5f5315e551266c25f1676a323f0cff9c1f91532
node0112 <siddheshthorat25@gmail.com>
2022-08-16 23:15:46 +0530

    created sidebox

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import './reset.css'
 import './site.css'
 import SignIn from './components/signIn';
-import firebase from "firebase/compat/app";
+import {signOut, getAuth} from 'firebase/auth'
 import { auth } from './fireabse'
 import { useEffect, useState } from 'react';
 import LoadingScreen from './components/LoadingScreen'
 
-import backgroundImage from './components/img1.jpg'
+import backgroundImage from './components/images/img1.jpg'
+import odlaw from './components/images/odlaw.webp'
+import waldo from './components/images/waldo.webp'
+import wenda from './components/images/wenda.webp'
+import wizard from './components/images/wizard.webp'
 import Header from './components/Header';
 
 
@@ -17,22 +21,7 @@ import Header from './components/Header';
 
 function App() {
 
-  const [user,setUser]=useState(()=>auth.currentUser)
-  const [initializing,setInitializing]=useState(true)
-  useEffect(()=>{
-    const unsubscribe= auth.onAuthStateChanged(user => {
-      if(user){
-        setUser(user)
-      }
-      else{
-        setUser(null)
-      }
-      if(initializing){
-        setInitializing(false)
-      }
-    })
-    return unsubscribe
-  },[])
+  
 
   let posX,posY,leftPos,topPos,modalX,modalY
 
@@ -49,7 +38,7 @@ function App() {
       topPos=posY-30
       modalX=leftPos+60 //places it right beside the selection square
       modalY=posY
-      if(topPos>headerHieght-10){
+      if(topPos>headerHieght-10){ //prevents square from rendering on header
         drawSquare()
         selectorRendered=true
       }
@@ -73,19 +62,77 @@ function App() {
     document.querySelector(".char-selector").classList.add('hide')
     selectorRendered=false
   }
+  let status='opened'
+  const handleSideBox = ()=>{document.querySelector('.character-card-opener').addEventListener('click',()=>{
+      let box=document.querySelector('.side-box')
+      let opener=document.querySelector('.character-card-opener')
+      if(status=='opened'){
+        box.style.left='-300px'
+        opener.style.transform='rotate(180deg)'
+        status='closed'
+      }
+      else{
+        box.style.left='0'
+        opener.style.transform='rotate(-0deg)'
+        status='opened'
+      }
+    })
+  }
+  
   //------------Firebase Functions Here----------------//
 
-  
+  const [user,setUser]=useState(()=>auth.currentUser)
+  const [initializing,setInitializing]=useState(true)
+  useEffect(()=>{
+    const unsubscribe= auth.onAuthStateChanged(user => {
+      if(user){
+        setUser(user)
+      }
+      else{
+        setUser(null)
+      }
+      if(initializing){
+        setInitializing(false)
+      }
+    })
+    return unsubscribe
+  },[])
+
+  function signOutUser() { //signs out user
+    // Sign out of Firebase.
+    signOut(getAuth());
+  }
 
   if(initializing) return <LoadingScreen loadingText={"Fetching Data From Server "}/>
 
   return (
     <div>
-      <Header />
-
+      <Header user={user}/>
       <div className='char-selector hide'></div>
       <div className='image-container'></div>
       <img src={backgroundImage} className="background-image"/>
+      <div className='side-box flex transition' onLoad={handleSideBox}>
+        <div className='character-box flex column transition'>
+          <div style={{fontFamily: 'open sans',fontSize: '1.5em',marginBottom: '20px'}}>Characters To Find:</div>
+          <div className='character-card flex'>
+            <img className='character-image' src={waldo}/>
+            <div className='character-text'>Waldo</div>
+          </div>
+          <div className='character-card flex'>
+            <img className='character-image' src={wenda}/>
+            <div className='character-text'>Wenda</div>
+          </div>
+          <div className='character-card flex'>
+            <img className='character-image' src={wizard}/>
+            <div className='character-text'>Wizard</div>
+          </div>
+          <div className='character-card flex'>
+            <img className='character-image' src={odlaw}/>
+            <div className='character-text'>Odlaw</div>
+          </div>
+        </div>
+        <div className='character-card-opener flex'>◀</div>
+      </div>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,22 @@
 import React from 'react'
+import SignIn from './signIn'
 
-function Header() {
+function Header({user}) {
   return (
     <div className="header" >
         <div className='logo'>Where's Waldo?</div>
         <ul  className='header-links'>
             <li><a className='transition header-link'>Repository</a></li>
             <li><a className='transition header-link' href=''>Projects+</a></li>
+            <li>{user ? <div className='profile'><img  className='profile-pic pointer transition' src={user.photoURL}></img>{user.displayName}
+            <div className='user-menu transition'>
+          <div style={{color: 'yellowgreen',fontFamily: 'open sans,sans-serif'}}>Saving Scores In Cloud</div>
+          <button className='menu-item transition'>Sign Out</button>
+          <button className='menu-item transition'></button>
+        </div>
+        </div> : <SignIn />}</li>
         </ul>
+
     </div>
   )
 }
